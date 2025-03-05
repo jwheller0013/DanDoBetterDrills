8478033dde153bf786e74cb738bbd2541f66e818
jwheller0013 <jwheller0013@yahoo.com>
2025-03-05 09:50:57 -0500

    complete getSeccondWord

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -86,7 +86,8 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String [] parts = spaceDelimitedString.split(" ");
+        return parts[1];
     }
 
     /**
